827f7814709893809736d7d47fa050d46bf71621
BerkeSaylan <berksay@yahoo.com>
2022-11-16 18:30:33 +0100

    TODOs added

--- src/de/tum/in/ase/Phone.java
+++ src/de/tum/in/ase/Phone.java
@@ -3,7 +3,7 @@ package de.tum.in.ase;
 public class Phone {
 
     //TODO: Add nextPhoneId, id, brand, price, touchscreen attributes to the Phone class and implement the constructor with these attributes
-    //TODO: Implement getter and setter methods for the non-static attributes
+    //TODO: Implement getter and setter methods for the attributes id, brand, price, touchscreen
     //TODO: Implement the toString method for the Phone class
 
 }
