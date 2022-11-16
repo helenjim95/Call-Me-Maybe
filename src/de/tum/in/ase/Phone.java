cb65d6668416eb10a1bdaac70056dc862bb72815
BerkeSaylan <berksay@yahoo.com>
2022-11-16 18:18:19 +0100

    TODOs added

--- src/de/tum/in/ase/Phone.java
+++ src/de/tum/in/ase/Phone.java
@@ -2,4 +2,8 @@ package de.tum.in.ase;
 
 public class Phone {
 
+    //TODO: Add nextPhoneId, id, brand, price, touchscreen attributes to the Phone class and implement the constructor with these attributes
+    //TODO: Implement getter and setter methods for the non-static attributes
+    //TODO: Implement the toString method for the Phone class
+
 }
--- src/de/tum/in/ase/PhoneFactory.java
+++ src/de/tum/in/ase/PhoneFactory.java
@@ -2,4 +2,15 @@ package de.tum.in.ase;
 
 public class PhoneFactory {
 
+    //TODO: Initialize the phoneList attribute (as an ArrayList or a LinkedList) in the constructor and add a getter, setter for this attribute
+    //TODO: Implement the producePhones method which adds the parameter phone to the phoneList
+    //TODO: Implement the findPhones method which searches through the entire phoneList and returns the phone with the matching id. If no such phone exists, return a new phone with the brand "", price -1 and without touchscreen feature
+    //TODO: Implement the removePhone method which removes the phone with the given id from the phoneList and returns the removed phone. If no phone with the given id exists, then follow the same rule in the findPhones method.
+    //TODO: Implement findTouchPhones method that iterates over the phoneList and store all touchscreen phones in a new list and return this list.
+    //TODO: Implement containsPhone that checks if the given phoneList contains the given phone. Return true if it does and false if it doesn't.
+    //TODO: Implement removeByBrand that removes all the phones with the brand from the list phones and returns a list that doesn't contain phones of this brand. If brand or phones is null, return an empty list.
+    //TODO: Implement findSmallestByLex method that returns the phone with the lexicographically smallest brand
+    //TODO: Implement sortByTouch that stores all the phones with a touchscreen in the beginning of a list, the rest of the phones at the end and return this list of phones.
+    //TODO: Implement sortByBrand that sorts the phones lexicographically according to their brand and return a list of sorted phones.
+    //TODO: Implement sortByPrice that sorts the list of phones according to their price in increasing order and return the sorted list
 }
