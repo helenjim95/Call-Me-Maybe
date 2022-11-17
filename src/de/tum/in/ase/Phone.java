e2fd79d0711932e658f603c22942995815c7e5d4
ge32goy <hanyin.chen@tum.de>
2022-11-17 22:53:41 +0100

    output

--- src/de/tum/in/ase/Phone.java
+++ src/de/tum/in/ase/Phone.java
@@ -1,9 +1,54 @@
 package de.tum.in.ase;
 
 public class Phone {
-
     //TODO: Add nextPhoneId, id, brand, price, touchscreen attributes to the Phone class and implement the constructor with these attributes
+    public int nextPhoneId;
+    private int id = -1;
+    private String brand;
+    private double price;
+    private boolean touchscreen;
     //TODO: Implement getter and setter methods for the attributes id, brand, price, touchscreen
+
+
+    public Phone(String brand, double price, boolean touchscreen) {
+        this.id += 1;
+        this.brand = brand;
+        this.price = price;
+        this.touchscreen = touchscreen;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public boolean isTouchscreen() {
+        return touchscreen;
+    }
+
+    public void setTouchscreen(boolean touchscreen) {
+        this.touchscreen = touchscreen;
+    }
+
     //TODO: Implement the toString method for the Phone class
 
 }
--- src/de/tum/in/ase/PhoneFactory.java
+++ src/de/tum/in/ase/PhoneFactory.java
@@ -1,16 +1,87 @@
 package de.tum.in.ase;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class PhoneFactory {
 
     //TODO: Initialize the phoneList attribute (as an ArrayList or a LinkedList) in the constructor and add a getter, setter for this attribute
+    private List<Phone> phoneList;
+    public PhoneFactory() {
+        this.phoneList = new LinkedList<>();
+    }
+
+    public List<Phone> getPhoneList() {
+        return phoneList;
+    }
+
+    public void setPhoneList(List<Phone> phoneList) {
+        this.phoneList = phoneList;
+    }
     //TODO: Implement the producePhones method which adds the parameter phone to the phoneList
+    public void producePhone(Phone phone) {
+        phoneList.add(phone);
+    }
     //TODO: Implement the findPhones method which searches through the entire phoneList and returns the phone with the matching id. If no such phone exists, return a new phone with the brand "", price -1 and without touchscreen feature
+    public Phone findPhone(int id) {
+        if (id < 0) {
+            return new Phone("", -1, false);
+        }
+        for (Phone phone : phoneList) {
+            if (phone.getId() == id) {
+                return phone;
+            }
+        }
+        return new Phone("", -1, false);
+    }
     //TODO: Implement the removePhone method which removes the phone with the given id from the phoneList and returns the removed phone. If no phone with the given id exists, then follow the same rule in the findPhones method.
+    public Phone removePhone(int id) {
+        Phone phone = findPhone(id);
+        phoneList.remove(phone);
+        return phone;
+    }
     //TODO: Implement findTouchPhones method that iterates over the phoneList and store all touchscreen phones in a new list and return this list.
+    public List<Phone> findTouchPhones() {
+        List<Phone> touchPhones = new LinkedList<>();
+        for (Phone phone : phoneList) {
+            if (phone.isTouchscreen()) {
+                touchPhones.add(phone);
+            }
+        }
+        return touchPhones;
+    }
     //TODO: Implement containsPhone that checks if the given phoneList contains the given phone. Return true if it does and false if it doesn't.
+    public boolean containsPhone(List<Phone> phoneList, Phone phone) {
+        return phoneList.contains(phone);
+    }
     //TODO: Implement removeByBrand that removes all the phones with the brand from the list phones and returns a list that doesn't contain phones of this brand. If brand or phones is null, return an empty list.
+    public List<Phone> removeByBrand(List<Phone> phoneList, String brand) {
+        if (brand == null || phoneList == null) {
+            return List.of();
+        }
+        for (Phone phone : phoneList) {
+            if (phone.getBrand().equals(brand)) {
+                phoneList.remove(phone);
+            }
+        }
+        return phoneList;
+    }
     //TODO: Implement findSmallestByLex method that returns the phone with the lexicographically smallest brand
+//    public Phone findSmallestByLex(List<Phone> phoneList) {
+//        return phoneList;
+//    }
     //TODO: Implement sortByTouch that stores all the phones with a touchscreen in the beginning of a list, the rest of the phones at the end and return this list of phones.
+    public List<Phone> sortByPrice() {
+        return phoneList;
+    }
     //TODO: Implement sortByBrand that sorts the phones lexicographically according to their brand and return a list of sorted phones.
+    public List<Phone> sortByTouch() {
+        List<Phone> outputList = new ArrayList<Phone>();
+        return phoneList;
+    }
     //TODO: Implement sortByPrice that sorts the list of phones according to their price in increasing order and return the sorted list
+    public List<Phone> sortByBrand() {
+        return phoneList;
+    }
 }
