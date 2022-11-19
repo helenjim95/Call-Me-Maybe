0c0dfefecbba45fe8bf0b48c02ee74befa5497c3
ge32goy <hanyin.chen@tum.de>
2022-11-19 10:31:28 +0100

    output

--- src/de/tum/in/ase/Phone.java
+++ src/de/tum/in/ase/Phone.java
@@ -2,20 +2,17 @@ package de.tum.in.ase;
 
 public class Phone {
     //Done: Add nextPhoneId, id, brand, price, touchscreen attributes to the Phone class and implement the constructor with these attributes
-    public static int nextPhoneId;
-    private int id;
+    public static int nextPhoneId = 1;
+    private int id = -1;
     private String brand;
     private double price;
     private boolean touchscreen;
-    private int count = 0;
     //Done: Implement getter and setter methods for the attributes id, brand, price, touchscreen
     public Phone(String brand, double price, boolean touchscreen) {
-        this.id = count;
-        nextPhoneId = this.id + 1;
+        this.id += nextPhoneId;
         this.brand = brand;
         this.price = price;
         this.touchscreen = touchscreen;
-        count += 1;
     }
 
     public int getId() {
